99ae32cb3fe4201392bbd78c8704065429a176db
dduoming <dltndudvldzm@naver.com>
2022-01-09 02:55:54 +0900

    spring setting

---
+++ src/test/java/com/warrior/ddamddam/DdamddamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warrior.ddamddam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdamddamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
