c17e75e4324698273d630c965aad696da073ad1c
dduoming <dltndudvldzm@naver.com>
2022-01-21 03:20:26 +0900

    공스타그램 cr 완료

---
+++ src/main/java/com/warrior/ddamddam/gongstagram/domain/Post.java
@@ -0,0 +1,36 @@
+package com.warrior.ddamddam.gongstagram.domain;
+
+
+import com.warrior.ddamddam.gongstagram.dto.PostDto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+public class Post extends Timestamped {
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @Column(name = "post_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Builder
+    public Post(PostDto postDto) {
+        this.content = postDto.getContent();
+
+    }
+    public void updateMyPost(PostDto postDto){
+        this.content= postDto.getContent();
+
+    }
+
+
+}
