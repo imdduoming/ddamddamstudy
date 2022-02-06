baa2f88fd7d2582f39d735a9b0678c57b243c537
dduoming <dltndudvldzm@naver.com>
2022-02-06 20:07:59 +0900

    [공스타그램] 댓글기능 구현

--- src/main/java/com/warrior/ddamddam/gongstagram/domain/Post.java
+++ src/main/java/com/warrior/ddamddam/gongstagram/domain/Post.java
@@ -1,6 +1,7 @@
 package com.warrior.ddamddam.gongstagram.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.warrior.ddamddam.gongstagram.dto.PostDto;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Setter
 @Getter
@@ -22,15 +24,24 @@ public class Post extends Timestamped {
     @Column(nullable = false)
     private String content;
 
+    @Column(nullable = false)
     private String image;
 
+    @Column(nullable = false)
     private String image_real;
+
+    @Column(nullable = false)
+    private int num_comment;
+
+    @JsonIgnore
+    @OneToMany(mappedBy="post", cascade = { CascadeType.PERSIST,CascadeType.REMOVE})
+    private List<Comment> comments;
 
     @Builder
     public Post(PostDto postDto) {
         this.content = postDto.getContent();
-
     }
+
     public void updateMyPost(PostDto postDto){
         this.content= postDto.getContent();
 
---
+++ src/main/java/com/warrior/ddamddam/gongstagram/dto/CommentDto.java
@@ -0,0 +1,9 @@
+package com.warrior.ddamddam.gongstagram.dto;
+
+import lombok.Getter;
+
+@Getter
+public class CommentDto {
+    private String content;
+    private Long idx; // 글 id
+}
